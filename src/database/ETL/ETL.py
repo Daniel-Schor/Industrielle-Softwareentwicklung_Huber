9223f8731ad2ee84c86dc101f65c5c4ebdc649ad
Daniel Schor <daniel.schor@gmx.de>
2025-02-01 12:19:13 +0100

    ETL update

--- src/database/ETL/ETL.py
+++ src/database/ETL/ETL.py
@@ -9,50 +9,63 @@ class Datei_Einlesen:
     def __init__(self, file_path: str):
         """Initialisiert die Klasse mit dem Pfad zur CSV-Datei."""
         self.file_path = os.path.abspath(file_path)
-        self.data_frame = pd.read_csv(
+        self.extract()
+
+        # Entfernt überflüssige Leerzeichen aus den Spaltennamen
+        self.df.columns = self.df.columns.str.strip()
+
+        self.transform()
+
+# extract ---------------------------------------------------------------------------------------------------------
+
+    def extract(self):
+        self.df = pd.read_csv(
             self.file_path,
             sep=",",  # Trennzeichen in der CSV-Datei
             encoding="utf-8-sig"  # Kodierung der Datei
         )
-        # Entfernt überflüssige Leerzeichen aus den Spaltennamen
-        self.data_frame.columns = self.data_frame.columns.str.strip()
 
-        # Zwei Dezimalstellen für numerische Werte (außer "Year", "Country" und "Region" Spalten)
-        required_columns = ["Year", "Country", "Region"]
-        for column in self.data_frame.columns:
-            if column not in required_columns:
-                self.data_frame[column] = pd.to_numeric(
-                    self.data_frame[column], errors='coerce')
-                self.data_frame[column] = self.data_frame[column].map(
-                    lambda x: f"{x:.2f}" if isinstance(x, (int, float)) else x)
+# transform ---------------------------------------------------------------------------------------------------------
+    def transform(self):
+        self.df.sort_values(by=["Year", "Country"], inplace=True)
 
-        # Methode um eine neue Spalte zu erzeugen, was die Summe von allen Spalten mit "Percentage" im Namen ist.
+        self.calc_net_income()
         self.add_sum_percentage_column()
+        self.calc_real_values()
 
-        # Spalten mit den Wörtern "Tax" oder "Percentage" werden als Prozent formatiert, die mit Cost oder Income mit $.
-        # Bei manchen Spalten sind sowohl das Wort "Tax" als "Cost" enthalten, da kommt dann die erste Bedingung.
-        for column in self.data_frame.columns:
-            if any(keyword in column for keyword in ["Tax", "Percentage"]):
-                self.data_frame[column] = self.data_frame[column].map(
-                    lambda x: f"{str(x)}%" if isinstance(x, (int, float, str)) else x)
-            elif any(keyword in column for keyword in ["Cost", "Income"]):
-                self.data_frame[column] = self.data_frame[column].map(
-                    lambda x: f"${str(x)}" if isinstance(x, (int, float, str)) else x)
-                
- # Methode um eine neue Spalte zu erzeugen, was die Summe von allen Spalten mit "Percentage" im Namen ist.
+    def calc_net_income(self):
+        self.df["Net_Income"] = self.df["Average_Monthly_Income"] * 0.01 * self.df["Tax_Rate"]
+        self.move_to_neighbour("Average_Monthly_Income", "Net_Income")
+    
+    def calc_real_values(self):
+        suffix = "_Percentage"
+        for column in self.df.columns:
+            if suffix in column:
+                self.df[column.removesuffix(suffix)] = self.df[column] * 0.01 * self.df["Net_Income"]
+                self.move_to_neighbour(column, column.removesuffix(suffix))
+    
+    def move_to_neighbour(self, column, new_neighbour):
+        neighbour_index = self.df.columns.get_loc(column)+1
+        self.df.insert(neighbour_index, new_neighbour, self.df.pop(new_neighbour))
+
+    # Methode um eine neue Spalte zu erzeugen, was die Summe von allen Spalten mit "Percentage" im Namen ist.
     def add_sum_percentage_column(self):
         """Erzeugt eine neue Spalte, die die Summe aller 'Percentage'-Spalten enthält."""
-        percentage_columns = [column for column in self.data_frame.columns if "Percentage" in column]
-        self.data_frame["Sum_Percentage"] = self.data_frame[percentage_columns].map(lambda x: float(str(x).replace('%', '')) if isinstance(x, str) else x).sum(axis=1)
-        self.data_frame["Sum_Percentage"] = self.data_frame["Sum_Percentage"].map(lambda x: f"{x:.2f}")
+        percentage_columns = [column for column in self.df.columns if "Percentage" in column]
+        self.df["Sum_Percentage"] = self.df[percentage_columns].map(lambda x: float(x) if isinstance(x, str) else x).sum(axis=1)
+        self.df["Sum_Percentage"] = self.df["Sum_Percentage"].map(lambda x: float(x))
+
+# load ---------------------------------------------------------------------------------------------------------
 
     # Methode um die Daten in einer SQLite-Datenbank zu speichern
     def save_to_db(self, db_name: str, table_name: str):
         """Speichert die Daten in einer SQLite-Datenbank."""
         with sqlite3.connect(db_name) as connection:
-            self.data_frame.to_sql(
+            self.df.to_sql(
                 table_name, connection, if_exists='replace', index=False)
             connection.commit()
+
+# other---------------------------------------------------------------------------------------------------------
 
     # Methode um die Daten aus der SQLite-Datenbank anzuzeigen. Ist eigentlich nicht notwendig, wir müssen entscheiden ob wir das drin lassen.
     def show_db(self, db_name: str, table_name: str):
@@ -71,10 +84,13 @@ class Datei_Einlesen:
         data = cursor.fetchall()
         conn.close()
         return data
+    
+# Main ---------------------------------------------------------------------------------------------------------
 
 if __name__ == "__main__":
     # Initialisiert die Verarbeitungsklasse mit dem Pfad zur CSV-Datei.
-    processor = Datei_Einlesen(r"Industrielle-Softwareentwicklung_Huber\src\database\ETL\data\CostOfLivingAndIncome.csv")
+    
+    processor = Datei_Einlesen("src/database/ETL/data/CostOfLivingAndIncome.csv")
 
     # Speichert die Daten in der SQLite-Datenbank.
     processor.save_to_db('Database1.db', 'CostOfLivingAndIncome')
