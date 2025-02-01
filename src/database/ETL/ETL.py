074c2d2723c5136eac8df72aff41401ae10e48ca
Daniel Schor <daniel.schor@gmx.de>
2025-02-01 15:32:51 +0100

    Füllen fehlender werte mit ETL

--- src/database/ETL/ETL.py
+++ src/database/ETL/ETL.py
@@ -50,20 +50,78 @@ class Datei_Einlesen:
         """
             Transformiert die Daten im DataFrame.
                 Transformationsprozess:
-            1. x
-            2. x
-            3. x
-
+            1. Berechnung des Nettoeinkommens
+            2. Berechnung der Summe der Prozentwerte
+            3. Berechnung der Dollar-Werte
+            4. Berechnung der Summe aller Kosten
+            4.5 Speichern der Regionen
+            5. Gruppierung nach Land und Jahr (aggregieren anhand des Mittelwerts)
+            6. Sortierung nach Land und Jahr
+            7. Füllen fehlender Werte mit linearer Interpolation
+            7.5 Füllen der Regionen
         """
 
-        self._df.sort_values(by=["Year", "Country"], inplace=True)
-
+        # 1.
         self.calc_net_income()
+
+        # 2.
         self.add_sum("Sum_Percentage", [column for column in self._df.columns if column.endswith(
             "Percentage") and column != "Savings_Percentage"])
+
+        # 3.
         self.calc_real_values()
+
+        # 4.
         self.add_sum(
             "Sum_Costs", [column for column in self._df.columns if column.endswith("Cost")])
+
+        # 4.5
+        region_dict = self._df.groupby("Country")["Region"].first().to_dict()
+
+        # 5.
+        self._df = self._df.groupby(
+            ["Country", "Year"], as_index=False).mean(numeric_only=True)
+        # 6.
+        self._df.sort_values(by=["Country", "Year"], inplace=True)
+
+        # 7.
+        self.fill_missing_values()
+
+        # 7.5
+        self._df["Region"] = self._df["Country"].map(region_dict)
+
+    def fill_missing_values(self) -> None:
+        """
+            Füllt fehlende Werte im DataFrame auf mit linearer Interpolation.
+        """
+
+        year_range = range(2000, 2024)
+
+        # Alle Länder
+        countries = self._df["Country"].unique()
+
+        # Combiation von allen Ländern und Jahren
+        all_combinations = pd.DataFrame(
+            [(country, year) for country in countries for year in year_range],
+            columns=["Country", "Year"]
+        )
+
+        # Einfügen der fehlenden Kombinationen
+        self._df = pd.merge(all_combinations, self._df, on=[
+                            "Country", "Year"], how="left")
+
+        # Spalten mit zahlenwerten
+        numeric_columns = [col for col in self._df.columns if col not in [
+            "Country", "Year", "Region"]]
+
+        # Füllen der fehlenden Werte
+        for column in numeric_columns:
+            self._df[column] = (
+                self._df.groupby("Country")[column]
+                # Füllen der fehlenden Werte mithilfe von linearer Interpolation
+                .apply(lambda group: group.interpolate(method="linear", limit_direction="both"))
+                .reset_index(level=0, drop=True)
+            )
 
     def calc_net_income(self) -> None:
         """
